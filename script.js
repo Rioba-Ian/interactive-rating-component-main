9d04e48d1561fd2195b167808ef98e068402ec80
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-09-14 13:59:56 +0300

    "submit  value to thankyou"

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-const ratingsToggle = document.getElementById("ratings")
-const thankyouToggle = document.getElementById("thankyou")
+const ratings = document.querySelectorAll(".select")
 const submitBtn = document.getElementById("submit")
+const score = document.querySelector(".aside-rating-btns")
+const scoreCard = document.getElementById("ratings")
+
+const thankyou = document.getElementById("thankyou")
+
+let answerValue = ''
+
+const result = document.querySelector(".result")
 
 console.log("I exist.");
 
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    ratingsToggle.style.display = "none"
-    thankyouToggle.style.display = "flex"
-    console.log("I have been clicked. ");
+ratings.forEach((rating, index) => {
+    rating.addEventListener("click", (e) => {
+        e.preventDefault()
+        console.log("clicked.");
+        const current = e.target.value
+        console.log(current);
+
+        if (current === 0) {
+            console.error("Nothing has been clicked");
+        }
+        else {
+            answerValue = current
+            console.log(answerValue);
+        }
+    })
 })
+
+function storeAnswer(answerValue) {
+    result.innerHTML = answerValue
+}
+
+submitBtn.addEventListener('click', () => {
+    scoreCard.style.display = 'none'
+    thankyou.style.display = 'flex'
+    storeAnswer(answerValue)
+    // console.log("it works");
+})
+
